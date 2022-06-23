ee97e8ac3056341c3687ee537cb36dd7e2469b56
junw6n <junw6ndev@gmail.com>
2022-06-23 13:42:16 +0900

    todolist add controller 추가

---
+++ src/main/java/controller/todolist/addController.java
@@ -0,0 +1,26 @@
+package controller.todolist;
+
+import service.todolistService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/todolist/add")
+public class addController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
+        todolistService service = new todolistService();
+
+        int result = service.addTodo(title, description);
+
+        if (result == 1) response.sendRedirect("/todolist");
+        else System.out.println("failed...");
+    }
+}
