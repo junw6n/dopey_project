f167e951f54521c499fe8c39ced5c68a12715d3e
junw6n <junw6ndev@gmail.com>
2022-08-16 15:55:33 +0900

    1. dbconnection -> datasource로 변경 2. jdbcTemplate 적용

--- src/main/java/domain/serviceFactory.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-import dbconnector.h2Connection;
-
-public class serviceFactory {
-    public TodoService todolistServiceFactroy() {
-        return new TodoService(new h2Connection());
-    }
-}
--- src/test/java/service/TodoServiceTest.java
+++ src/test/java/service/TodoServiceTest.java
@@ -1,18 +1,26 @@
 package service;
 
-import dbconnector.dbConnection;
-import dbconnector.h2Connection;
 import domain.Todo;
 import domain.TodoService;
 import domain.TodoStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = "file:web/WEB-INF/dispatcher-servlet.xml")
 class TodoServiceTest {
+    @Autowired
     private TodoService service;
     private Todo todo1;
     private Todo todo2;
@@ -25,26 +33,24 @@ class TodoServiceTest {
 
     @BeforeEach
     void setUp() {
-        dbConnection dbconn = new h2Connection();
-        this.service = new TodoService(dbconn);
         this.todo1 = new Todo(1, "work out - 22.06.21", "legs", TodoStatus.IDLE);
         this.todo2 = new Todo(2, "work out - 22.06.23", "chest, arms", TodoStatus.IDLE);
         this.todo3 = new Todo(3, "read book 2", "\"object-oriented programming\"", TodoStatus.IDLE);
     }
     @Test
     void allToDoList() {
-        assertEquals(initCount, service.allToDoList().size());
+        assertEquals(initCount, service.getAll().size());
 
-        ArrayList<Todo> test = new ArrayList<>();
+        List<Todo> test = new ArrayList<>();
         test.add(this.todo1);
         test.add(this.todo2);
 
         // add
         test.forEach(todo -> {
-            service.addTodo(todo.getTitle(), todo.getDescription());
+            service.add(todo);
         });
 
-        ArrayList<Todo> list = service.allToDoList();
+        List<Todo> list = service.getAll();
 
         assertEquals(initCount + 2, list.size());
         assertTrue(sameTodo(this.todo1, list.get(list.size()-2)));
@@ -52,10 +58,10 @@ class TodoServiceTest {
 
         // remove
         list.subList(list.size() - 2, list.size()).forEach(todo -> {
-            service.removeTodo(todo.getId());
+            service.remove(todo);
         });
 
-        assertEquals(initCount, service.allToDoList().size());
+        assertEquals(initCount, service.getAll().size());
     }
 
     @Test
